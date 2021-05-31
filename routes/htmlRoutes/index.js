2d9dd486063357209ec695044a7c347bc03442a8
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-05-30 19:08:49 -0700

    fixed typo on HTML routing

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public.notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 router.get("*", (req, res) => {
